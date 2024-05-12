466e580be7da10d6cab9c8b77044aca3d81ad88a
lawrence307 <lawrence_muthini@yahoo.com>
2024-05-12 08:57:20 -0700

    debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
- bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# Fixes Apache 500 error
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
